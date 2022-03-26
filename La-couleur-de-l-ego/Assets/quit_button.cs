05deacf1a052d6930df9bd7c7901b13e4646b7c2
Mickaël <72012119+Triencli@users.noreply.github.com>
2022-03-26 17:12:21 +0100

    menu

---
+++ La-couleur-de-l-ego/Assets/quit_button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quit_button : MonoBehaviour {
+    public Text Text;
+    public void setText())
+    {
+        Text.text = "-42?";
+    }
+}
