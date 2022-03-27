d8821b438bd44062f06cbff1ee04aa3064d8ac02
Jeremie Joannard <Samael@localhost.localdomain>
2022-03-27 01:28:41 +0100

    [ADD]Bullet can be fired from anywhere

--- La-couleur-de-l-ego/Assets/Scripts/bullet.cs
+++ La-couleur-de-l-ego/Assets/Scripts/bullet.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 2f;
     public int damage = 40;
     public Rigidbody2D rb;
     // Start is called before the first frame update
+
+    //set the bullet's direction and angle to the mouse cursor's position
     void Start()
     {
-        rb.velocity = transform.right * speed;
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        rb.velocity = direction * speed;
     }
-
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         Ennemy enemy = hitInfo.GetComponent<Ennemy>();
@@ -25,6 +32,7 @@ public class bullet : MonoBehaviour
         {
             bigFish.TakeDamage(damage);
         }
-        Destroy(gameObject);
+        if (hitInfo.GetComponent<Player>() == null)
+            Destroy(gameObject);
     }
 }
