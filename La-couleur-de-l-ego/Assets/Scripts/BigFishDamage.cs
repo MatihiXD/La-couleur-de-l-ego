0d2fcd15cbb2fc2ed0014bf75f1952f53df1ed2d
Jeremie Joannard <Samael@localhost.localdomain>
2022-03-27 13:45:41 +0200

    [ADD]Fish death

--- La-couleur-de-l-ego/Assets/Scripts/BigFishDamage.cs
+++ La-couleur-de-l-ego/Assets/Scripts/BigFishDamage.cs
@@ -6,13 +6,19 @@ public class BigFishDamage : MonoBehaviour
 {
     public int health = 100;
     public int damage = 10;
-
+    private GameObject DieEvent;
+    void Start() {
+        DieEvent = Resources.Load("Prefab/EnemyDeath") as GameObject;
+    }
     public void TakeDamage(int damage)
     {
         health -= damage;
-        if (health <= 0)
+        if (health <= 0) {
+            Instantiate(DieEvent, transform.position, Quaternion.identity);
             Die();
+        }
     }
+
 
     void Die()
     {
--- La-couleur-de-l-ego/Assets/Scripts/Ennemy.cs
+++ La-couleur-de-l-ego/Assets/Scripts/Ennemy.cs
@@ -7,12 +7,18 @@ public class Ennemy : MonoBehaviour
 
     public int health = 100;
     public int damage = 10;
-
+    
+    private GameObject DieEvent;
+    void Start() {
+        DieEvent = Resources.Load("Prefab/EnemyDeath") as GameObject;
+    }
     public void TakeDamage(int damage)
     {
         health -= damage;
-        if (health <= 0)
+        if (health <= 0) {
+            Instantiate(DieEvent, transform.position, Quaternion.identity);
             Die();
+        }
     }
 
     void Die()
